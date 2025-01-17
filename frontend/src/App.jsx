deea3bbd53c1c80a7474c1c7efa1e2d01ea83e8a
deepanshu <deepanshukumar2022@gmail.com>
2025-01-17 22:48:12 +0530

    here we complete LOGIN & Signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home/home'
 import Cart from './pages/cart/cart'
 import Placeorder from './pages/Placeorder/Placeorder'
 import Footer from './components/Footer/Footer'
+import LoginPopUp from './components/LoginPopUp/LoginPopUp'
 
 
 const App = () => {
+
+  const  [showLogin,setShowLogin] = useState(false)
   return (
     <>
+    {showLogin?<LoginPopUp setShowLogin={setShowLogin}/>:<></>}
     <div className='app'>
-      <Navbar/>
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
        <Route path='' element={<Home/>}/>
        <Route path='/cart' element={<Cart/>}/>
