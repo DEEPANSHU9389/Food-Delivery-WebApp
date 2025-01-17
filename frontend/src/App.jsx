0eae46a1235f0ede357fd2306fbfa4b1acc2afaf
deepanshu <deepanshukumar2022@gmail.com>
2025-01-17 21:30:04 +0530

    Now here we complete Home page with resposnive any screen size

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,24 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home/home'
 import Cart from './pages/cart/cart'
 import Placeorder from './pages/Placeorder/Placeorder'
+import Footer from './components/Footer/Footer'
 
 
 const App = () => {
   return (
+    <>
     <div className='app'>
       <Navbar/>
       <Routes>
        <Route path='' element={<Home/>}/>
        <Route path='/cart' element={<Cart/>}/>
        <Route path='/order' element={<Placeorder/>}/>
-       
+
       </Routes>
       
     </div>
+    <Footer/>
+    </>
   )
 }
 
