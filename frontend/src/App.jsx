eafe6d10147d104d6df78ba0c15ba04ab10c78b8
deepanshu <deepanshukumar2022@gmail.com>
2025-01-17 15:18:57 +0530

    here we create all pages Cart, Home , PlaceOrder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home/home'
+import Cart from './pages/cart/cart'
+import Placeorder from './pages/Placeorder/Placeorder'
+
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+       <Route path='' element={<Home/>}/>
+       <Route path='/cart' element={<Cart/>}/>
+       <Route path='/order' element={<Placeorder/>}/>
+       
+      </Routes>
       
     </div>
   )
